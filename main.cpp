a8c486567a3c0523772702a0849df69badc91a61
Bucky Kittinger <larrykittinger@gmail.com>
2019-04-19 20:54:09 -0400

    working on integration

--- main.cpp
+++ main.cpp
@@ -6,8 +6,9 @@
 using namespace eosio;
 using namespace eosio::wasm_backend;
 
-void print(int a, int b, int c, int d, int e, int f, int g) {
-   std::cout << "PRINT " << a << " " << b << " " << c << " " << d << " " << e << " " << f << " " << g << "\n";
+void print(const std::string& s) {
+   //std::cout << "PRINT " << a << " " << b << " " << c << " " << d << " " << e << " " << f << " " << g << "\n";
+   std::cout << "PRINT " << s << "\n";
 }
 
 registered_function<&print, decltype("env"_hfn), decltype("print"_hfn), backend> _rf;
@@ -17,9 +18,8 @@ int main(int argc, char** argv) {
    auto code = backend::read_wasm( argv[1] );
    auto t1 = std::chrono::high_resolution_clock::now();
    eosio::wasm_backend::backend bkend( code, wa );
-   eosio::wasm_backend::registered_host_functions::add<print>("env", "print");
-
-   /*
+   eosio::wasm_backend::registered_host_functions::add<print, eosio::wasm_backend::backend>("env", "print");
+   eosio::wasm_backend::registered_host_functions::resolve( bkend.get_module() );
    try {
       bkend.execute_all();
    } catch ( const wasm_interpreter_exception& ex ) {
@@ -29,7 +29,7 @@ int main(int argc, char** argv) {
    } catch ( const wasm_memory_exception& ex ) {
       std::cerr << ex.what() << " : " << ex.detail() << "\n";
    }
-   */
+
    std::cout << "INSTRUCTIONS : " << bkend.get_instructions() << "\n";
    auto t2 = std::chrono::high_resolution_clock::now();
    std::cout << std::chrono::duration_cast<std::chrono::nanoseconds>(t2-t1).count() << '\n';
