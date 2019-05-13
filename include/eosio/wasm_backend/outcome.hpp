b6449c80d433476d9eeff458f15542f3a51ecfec
Bucky Kittinger <larrykittinger@gmail.com>
2019-05-13 17:12:19 -0400

    forgot file

---
+++ include/eosio/wasm_backend/outcome.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+/// wrapper header to use either the built in outcome library or an external Boost v1.70 outcome library
+#ifdef EOSIO_USE_EXTERNAL_OUTCOME
+#include <outcome/single-header/outcome.hpp>
+#else
+//#include <
+#endif
