89ec87dc35095f75f209a230d5febc66585fb918
Bucky Kittinger <larry.kittinger@block.one>
2019-05-17 01:25:56 -0400

    more changes for outcome

--- include/eosio/vm/dispatcher.hpp
+++ include/eosio/vm/dispatcher.hpp
@@ -3,11 +3,12 @@
 #include <eosio/vm/variant.hpp>
 #include <eosio/vm/opcodes.hpp>
 
-#define DISPATCH(VISITOR, VARIANT) \
-   {  \
-      decltype(VISITOR)& __eos_vm_visitor = VISITOR; \
-      decltype(VARIANT)& __eos_vm_variant = VARIANT ; \
-      static void* dispatch_table[] = { \
+// clang-format off
+#define DISPATCH(VISITOR, VARIANT)                       \
+   {                                                     \
+      decltype(VISITOR)& __eos_vm_visitor = VISITOR;     \
+      decltype(VARIANT)& __eos_vm_variant = VARIANT ;    \
+      static void* dispatch_table[] = {                  \
       CONTROL_FLOW_OPS(CREATE_TABLE_ENTRY)               \
       BR_TABLE_OP(CREATE_TABLE_ENTRY)                    \
       RETURN_OP(CREATE_TABLE_ENTRY)                      \
@@ -24,21 +25,23 @@
       CONVERSION_OPS(CREATE_TABLE_ENTRY)                 \
       SYNTHETIC_OPS(CREATE_TABLE_ENTRY)                  \
       ERROR_OPS(CREATE_TABLE_ENTRY)                      \
-      }; \
-      CONTROL_FLOW_OPS(CREATE_LABEL) \
-      BR_TABLE_OP(CREATE_LABEL) \
-      RETURN_OP(CREATE_LABEL) \
-      CALL_OPS(CREATE_LABEL) \
-      PARAMETRIC_OPS(CREATE_LABEL) \
-      VARIABLE_ACCESS_OPS(CREATE_LABEL) \
-      MEMORY_OPS(CREATE_LABEL) \
-      I32_CONSTANT_OPS(CREATE_LABEL) \
-      I64_CONSTANT_OPS(CREATE_LABEL) \
-      F32_CONSTANT_OPS(CREATE_LABEL) \
-      F64_CONSTANT_OPS(CREATE_LABEL) \
-      COMPARISON_OPS(CREATE_LABEL) \
-      NUMERIC_OPS(CREATE_LABEL) \
-      CONVERSION_OPS(CREATE_LABEL) \
-      SYNTHETIC_OPS(CREATE_LABEL) \
-      ERROR_OPS(CREATE_LABEL) \
+      };                                                 \
+      CONTROL_FLOW_OPS(CREATE_LABEL)                     \
+      BR_TABLE_OP(CREATE_LABEL)                          \
+      RETURN_OP(CREATE_LABEL)                            \
+      CALL_OPS(CREATE_LABEL)                             \
+      PARAMETRIC_OPS(CREATE_LABEL)                       \
+      VARIABLE_ACCESS_OPS(CREATE_LABEL)                  \
+      MEMORY_OPS(CREATE_LABEL)                           \
+      I32_CONSTANT_OPS(CREATE_LABEL)                     \
+      I64_CONSTANT_OPS(CREATE_LABEL)                     \
+      F32_CONSTANT_OPS(CREATE_LABEL)                     \
+      F64_CONSTANT_OPS(CREATE_LABEL)                     \
+      COMPARISON_OPS(CREATE_LABEL)                       \
+      NUMERIC_OPS(CREATE_LABEL)                          \
+      CONVERSION_OPS(CREATE_LABEL)                       \
+      SYNTHETIC_OPS(CREATE_LABEL)                        \
+      ERROR_OPS(CREATE_LABEL)                            \
    }
+
+// clang-format on
