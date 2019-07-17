8334bf9296771fe545f891c72b157753b84b458f
Nathan Pierce <nathan.pierce@block.one>
2019-07-17 12:35:21 -0400

    revert

--- .cicd/generate-base-images.sh
+++ .cicd/generate-base-images.sh
@@ -4,8 +4,8 @@ cd $( dirname "${BASH_SOURCE[0]}" )/.. # Ensure we're in the repo root and not i
 . ./.cicd/.helpers
 
 [[ -z $DETERMINED_HASH ]] && echo "DETERMINED_HASH empty! (check determine-hash function)" && exit 1
-DOCKER_CLI_EXPERIMENTAL=enabled # needed for docker manifest inspect
-if docker manifest inspect $FULL_TAG; then
+echo "Looking for $IMAGE_TAG-$DETERMINED_HASH"
+if docker pull $FULL_TAG; then
     echo "$FULL_TAG already exists"
 else
     generate_docker_image
