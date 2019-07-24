11175303e9527df4b2e0237126e27a36cc44fc6d
Bucky Kittinger <larry.kittinger@block.one>
2019-07-24 16:21:16 -0400

    updated tests

--- tests/watchdog_tests.cpp
+++ tests/watchdog_tests.cpp
@@ -1,31 +1,26 @@
 #include <eosio/vm/watchdog.hpp>
 #include <chrono>
 
-#define BOOST_TEST_MAIN
-#include <boost/test/included/unit_test.hpp>
+#include <catch2/catch.hpp>
 
 using eosio::vm::watchdog;
 
-BOOST_AUTO_TEST_SUITE(watchdog_tests)
-
-BOOST_AUTO_TEST_CASE(watchdog_interrupt) {
+TEST_CASE("watchdog interrupt", "[watchdog_interrupt]") {
   std::atomic<bool> okay = false;
   watchdog w{std::chrono::milliseconds(50)};
   {
     auto g = w.scoped_run([&]() { okay = true; });
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
   }
-  BOOST_TEST(okay);
+  CHECK(okay);
 }
 
-BOOST_AUTO_TEST_CASE(watchdog_no_interrupt) {
+TEST_CASE("watchdog no interrupt", "[watchdog_no_interrupt]") {
   std::atomic<bool> okay = true;
   watchdog w{std::chrono::milliseconds(50)};
   {
     auto g = w.scoped_run([&]() { okay = false; });
   } // the guard goes out of scope here, cancelling the timer
   std::this_thread::sleep_for(std::chrono::milliseconds(100));
-  BOOST_TEST(okay);
+  CHECK(okay);
 }
-
-BOOST_AUTO_TEST_SUITE_END()
