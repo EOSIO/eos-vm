fc1ca2dc03f6a1d2a8f0b6e775cccf27c842a7fc
Zach Butler <zach.butler@block.one>
2019-07-29 17:31:02 -0400

    Renamed helper script: no longer hidden, IDEs provide syntax highlighting by default

--- .cicd/travis-build.sh
+++ .cicd/travis-build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -eo pipefail
 cd $( dirname "${BASH_SOURCE[0]}" )/.. # Ensure we're in the repo root and not inside of scripts
-. ./.cicd/.helpers
+. ./.cicd/helpers.sh
 execute docker run --rm -v $(pwd):/workdir -v /usr/lib/ccache -v $HOME/.ccache:/opt/.ccache -e CCACHE_DIR=/opt/.ccache $FULL_TAG
