b8e00338ad45257a7f1800f82dea709b85bb8b74
Zach Butler <zach.butler@block.one>
2019-07-29 18:04:28 -0400

    Simplification: Removed docker_tag_exists()

--- .cicd/generate-base-images.sh
+++ .cicd/generate-base-images.sh
@@ -1,10 +1,13 @@
-#!/usr/bin/env bash
+#!/bin/bash
 set -eo pipefail
-cd $( dirname "${BASH_SOURCE[0]}" )/.. # Ensure we're in the repo root and not inside of scripts
 . ./.cicd/helpers.sh
-echo "Looking for $FULL_TAG"
-if [[ docker_tag_exists $FULL_TAG ]]; then
-    echo "$FULL_TAG already exists"
+# look for Docker image
+echo "+++ :mag_right: Looking for $FULL_TAG"
+ORG_REPO=$(echo $FULL_TAG | cut -d: -f1)
+TAG=$(echo $FULL_TAG | cut -d: -f2)
+EXISTS=$(curl -s -H "Authorization: Bearer $(curl -sSL "https://auth.docker.io/token?service=registry.docker.io&scope=repository:${ORG_REPO}:pull" | jq --raw-output .token)" "https://registry.hub.docker.com/v2/${ORG_REPO}/manifests/$TAG")
+if [[ $EXISTS =~ '404 page not found' || $EXISTS =~ 'manifest unknown' ]]; then
+    echo "$FULL_TAG already exists."
 else # if we cannot pull the image, we build and push it first
     docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_PASSWORD
     cd ./.cicd
