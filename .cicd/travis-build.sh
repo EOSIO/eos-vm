f8085c245d1e6aaa92ec973a7593a97cf9df820e
Scott Arnette <scott.a.arnette@gmail.com>
2019-07-30 17:11:14 -0400

    Testing skipping org builds.

--- .cicd/travis-build.sh
+++ .cicd/travis-build.sh
@@ -10,5 +10,10 @@ function execute()
 
 echo "travis_repo_slug == $TRAVIS_REPO_SLUG"
 echo "travis_pr_branch == $TRAVIS_PULL_REQUEST_BRANCH"
-exit 0
-# execute docker run --rm -v $(pwd):/workdir -v /usr/lib/ccache -v $HOME/.ccache:/opt/.ccache -e CCACHE_DIR=/opt/.ccache $FULL_TAG
+if [[ $TRAVIS_REPO_SLUG -eq "$ORG" ]]; then
+  echo "Internal build, please see Buildkite."
+  exit 0
+else
+  echo "Launching build..."
+  # execute docker run --rm -v $(pwd):/workdir -v /usr/lib/ccache -v $HOME/.ccache:/opt/.ccache -e CCACHE_DIR=/opt/.ccache $FULL_TAG
+fi
