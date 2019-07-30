188c55774fac07c1153f47740cecb4a4bf1b5319
Nathan Pierce <nathan.pierce@block.one>
2019-07-30 14:19:00 -0400

    wrap mac in travis-build

--- .cicd/travis-build.sh
+++ .cicd/travis-build.sh
@@ -7,4 +7,10 @@ function execute() {
   "$@"
 }
 
-execute docker run --rm -v $(pwd):/workdir -v /usr/lib/ccache -v $HOME/.ccache:/opt/.ccache -e CCACHE_DIR=/opt/.ccache $FULL_TAG
+if [[ "$(uname)" == Darwin ]]; then
+  ccache -s
+  mkdir -p build && cd build && cmake ..
+  make -j$(getconf _NPROCESSORS_ONLN)
+else # linux
+  execute docker run --rm -v $(pwd):/workdir -v /usr/lib/ccache -v $HOME/.ccache:/opt/.ccache -e CCACHE_DIR=/opt/.ccache $FULL_TAG
+fi
