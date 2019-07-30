86832fabf0ce4739bcf1fbf69662716dde88dcaa
Scott Arnette <scott.a.arnette@gmail.com>
2019-07-30 17:00:52 -0400

    Testing Travis provided ENVs.

--- .cicd/travis-build.sh
+++ .cicd/travis-build.sh
@@ -8,4 +8,7 @@ function execute()
   "$@"
 }
 
-execute docker run --rm -v $(pwd):/workdir -v /usr/lib/ccache -v $HOME/.ccache:/opt/.ccache -e CCACHE_DIR=/opt/.ccache $FULL_TAG
+echo "travis_repo_slug == $TRAVIS_REPO_SLUG"
+echo "travis_pr_branch == $TRAVIS_PULL_REQUEST_BRANCH"
+exit 0
+# execute docker run --rm -v $(pwd):/workdir -v /usr/lib/ccache -v $HOME/.ccache:/opt/.ccache -e CCACHE_DIR=/opt/.ccache $FULL_TAG
