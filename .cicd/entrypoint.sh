cc14159fe661e77bb425357d3f7b4750251b8b78
Zach Butler <zach.butler@block.one>
2019-07-31 18:25:01 -0400

    Supply container entrypoint as script

---
+++ .cicd/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd /workdir
+.cicd/execute.sh
+echo '+++ :git: Updating Submodules'
+execute git submodule update --init --recursive
+echo '+++ :hammer: Building eos-vm'
+mkdir build
+execute cd /Workdir/build
+execute ccache -s
+execute cmake -DCMAKE_TOOLCHAIN_FILE=/workdir/.cicd/clang.make -DENABLE_TESTS=ON ..
+execute make -j $(nproc)
+echo '+++ :white_check_mark: Done!'
