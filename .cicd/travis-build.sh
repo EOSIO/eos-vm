070af2a8114df4e8eeb9f7c1320424d1cfcebb5d
Scott Arnette <scott.a.arnette@gmail.com>
2019-07-31 10:11:35 -0400

    Removed ENVs from build script.

--- .cicd/travis-build.sh
+++ .cicd/travis-build.sh
@@ -8,11 +8,4 @@ function execute()
   "$@"
 }
 
-if [[ $TRAVIS_REPO_SLUG == $ORG ]]; then
-  echo "travis_repo_slug == $TRAVIS_REPO_SLUG"
-  echo "org == $ORG"
-  echo "Internal build, please see Buildkite."
-  exit 0
-else
-  execute docker run --rm -v $(pwd):/workdir -v /usr/lib/ccache -v $HOME/.ccache:/opt/.ccache -e CCACHE_DIR=/opt/.ccache $FULL_TAG
-fi
+execute docker run --rm -v $(pwd):/workdir -v /usr/lib/ccache -v $HOME/.ccache:/opt/.ccache -e CCACHE_DIR=/opt/.ccache $FULL_TAG
