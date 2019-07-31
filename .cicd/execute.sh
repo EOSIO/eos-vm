dd506c329e5d1e14d85b01c7d6d0dcd1952a65a1
Zach Butler <zach.butler@block.one>
2019-07-31 16:07:07 -0400

    Moved execute() into library

---
+++ .cicd/execute.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+function execute()
+{
+  echo "$ $@"
+  "$@"
+}
--- .cicd/travis-build.sh
+++ .cicd/travis-build.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 set -eo pipefail
+. ./.cicd/execute.sh
 . ./.cicd/docker-hash.sh
-
-function execute()
-{
-  echo "$@"
-  "$@"
-}
-
 execute docker run --rm -v $(pwd):/workdir -v /usr/lib/ccache -v $HOME/.ccache:/opt/.ccache -e CCACHE_DIR=/opt/.ccache $FULL_TAG
