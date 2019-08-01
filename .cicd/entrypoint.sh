61ca4f80118b711dd2da6ce6fd511751f17700aa
Zach Butler <zach.butler@block.one>
2019-08-01 10:49:24 -0400

    Case matters on Linux, unlike macOS

--- .cicd/entrypoint.sh
+++ .cicd/entrypoint.sh
@@ -5,7 +5,7 @@ echo '+++ :git: Updating Submodules'
 execute git submodule update --init --recursive
 echo '+++ :hammer: Building eos-vm'
 mkdir build
-execute cd /Workdir/build
+execute cd /workdir/build
 execute ccache -s
 execute cmake -DCMAKE_TOOLCHAIN_FILE=/workdir/.cicd/clang.make -DENABLE_TESTS=ON ..
 execute make -j $(nproc)
