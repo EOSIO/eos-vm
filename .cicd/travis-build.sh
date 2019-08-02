c3d26a7a368e5d2fe5d820f1bf467c408fec033c
Zach Butler <zach.butler@block.one>
2019-08-02 11:34:20 -0400

    Resolve merge conflicts between 'trav-poc' and 'zach-simplification'

--- .cicd/travis-build.sh
+++ .cicd/travis-build.sh
@@ -2,4 +2,12 @@
 set -eo pipefail
 . ./.cicd/execute.sh
 . ./.cicd/docker-hash.sh
-execute docker run --rm -v $(pwd):/workdir -v /usr/lib/ccache -v $HOME/.ccache:/opt/.ccache -e CCACHE_DIR=/opt/.ccache $FULL_TAG
+if [[ "$(uname)" == Darwin ]]; then
+	ccache -s
+	mkdir build
+	cd build
+	cmake ..
+	make -j $(nproc)
+else # linux
+	execute docker run --rm -v $(pwd):/workdir -v /usr/lib/ccache -v $HOME/.ccache:/opt/.ccache -e CCACHE_DIR=/opt/.ccache $FULL_TAG
+fi
