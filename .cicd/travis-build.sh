1a353603d2e4f1b7b6f40dad8ed5231172b9c6f3
Zach Butler <zach.butler@block.one>
2019-08-02 11:38:02 -0400

    travis-build.sh: Add execute() to build steps

--- .cicd/travis-build.sh
+++ .cicd/travis-build.sh
@@ -3,11 +3,11 @@ set -eo pipefail
 . ./.cicd/execute.sh
 . ./.cicd/docker-hash.sh
 if [[ "$(uname)" == Darwin ]]; then
-	ccache -s
 	mkdir build
 	cd build
-	cmake ..
-	make -j $(nproc)
+	execute ccache -s
+	execute cmake ..
+	execute make -j $(nproc)
 else # linux
 	execute docker run --rm -v $(pwd):/workdir -v /usr/lib/ccache -v $HOME/.ccache:/opt/.ccache -e CCACHE_DIR=/opt/.ccache $FULL_TAG
 fi
