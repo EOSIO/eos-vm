974476925afbb61568db4d41582b03780ab29c4b
Scott Arnette <scott.a.arnette@gmail.com>
2019-08-05 09:42:37 -0400

    Testing CI platform ENVs.

--- .cicd/travis-build.sh
+++ .cicd/travis-build.sh
@@ -2,13 +2,18 @@
 set -eo pipefail
 . ./.cicd/execute.sh
 . ./.cicd/docker-hash.sh
-export JOBS="$(getconf _NPROCESSORS_ONLN)"
-if [[ "$(uname)" == Darwin ]]; then
-	mkdir build
-	cd build
-	execute ccache -s
-	execute cmake ..
-	execute make -j$JOBS
-else # linux
-	execute docker run --rm -v $(pwd):/workdir -v /usr/lib/ccache -v $HOME/.ccache:/opt/.ccache -e JOBS -e CCACHE_DIR=/opt/.ccache $FULL_TAG
-fi
+
+if [[ $TRAVIS ]]; then
+  echo "This build is running on TravisCI."
+elif [[ $BUILDKITE ]]; then
+  echo "This build is running on Buildkite."
+else
+  echo "Shouldn't get here."
+fi  
+# if [[ "$(uname)" == Darwin ]]; then
+#   ccache -s
+#   mkdir -p build && cd build && cmake ..
+#   make -j$(getconf _NPROCESSORS_ONLN)
+# else # linux
+#   execute docker run --rm -v $(pwd):/workdir -v /usr/lib/ccache -v $HOME/.ccache:/opt/.ccache -e CCACHE_DIR=/opt/.ccache $FULL_TAG
+# fi
