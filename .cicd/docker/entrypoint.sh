5b00ab80bbbff64ee516bbaf2d63a7567e0e4727
Nathan Pierce <nathan.pierce@block.one>
2019-08-05 15:26:22 -0400

    CMAKE_EXTRAS

--- .cicd/docker/entrypoint.sh
+++ .cicd/docker/entrypoint.sh
@@ -7,7 +7,8 @@ echo '+++ :hammer: Building eos-vm'
 mkdir -p build
 execute cd /workdir/build
 execute ccache -s
-execute cmake -DCMAKE_TOOLCHAIN_FILE=/workdir/.cicd/clang.make -DENABLE_TESTS=ON ..
+[[ ! $(cat /etc/issue) =~ "Ubuntu 18.04" ]] && CMAKE_EXTRAS="-DCMAKE_TOOLCHAIN_FILE=/workdir/.cicd/clang.make"
+execute cmake $CMAKE_EXTRAS -DENABLE_TESTS=ON ..
 execute make -j$JOBS
 if ${ENABLE_TESTS:-true}; then execute ctest -j$JOBS -V --output-on-failure -T Test; fi
 echo '+++ :white_check_mark: Done!'
