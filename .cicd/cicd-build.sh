f4fcc02ae44eab4d6884822fc688e9d49466496c
Scott Arnette <scott.a.arnette@gmail.com>
2019-08-05 12:04:34 -0400

    Removed echos.

--- .cicd/cicd-build.sh
+++ .cicd/cicd-build.sh
@@ -4,7 +4,6 @@ set -eo pipefail
 . ./.cicd/docker-hash.sh
 
 if [[ $TRAVIS ]]; then
-  echo "This build is running on TravisCI."
   export JOBS="$(getconf _NPROCESSORS_ONLN)"
   if [[ "$(uname)" == Darwin ]]; then
     ccache -s
@@ -14,10 +13,8 @@ if [[ $TRAVIS ]]; then
     execute docker run --rm -v $(pwd):/workdir -v /usr/lib/ccache -v $HOME/.ccache:/opt/.ccache -e JOBS -e CCACHE_DIR=/opt/.ccache $FULL_TAG
   fi
 elif [[ $BUILDKITE ]]; then
-  echo "This build is running on Buildkite."
   execute ./.cicd/generate-base-images.sh
   execute docker run --rm -v $(pwd):/workdir -e JOBS $FULL_TAG
 else
-  echo "Shouldn't get here."
   exit 1
 fi  
