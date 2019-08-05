5659a9d95e3b1f7a72dbc8609b220654f055e9a5
Scott Arnette <scott.a.arnette@gmail.com>
2019-08-05 10:10:09 -0400

    Testing build logic.

--- .cicd/travis-build.sh
+++ .cicd/travis-build.sh
@@ -5,15 +5,17 @@ set -eo pipefail
 
 if [[ $TRAVIS ]]; then
   echo "This build is running on TravisCI."
+  export JOBS="$(getconf _NPROCESSORS_ONLN)"
+  if [[ "$(uname)" == Darwin ]]; then
+    ccache -s
+    mkdir -p build && cd build && cmake ..
+    make -j$JOBS
+  else # linux
+    execute docker run --rm -v $(pwd):/workdir -v /usr/lib/ccache -v $HOME/.ccache:/opt/.ccache -e JOBS -e CCACHE_DIR=/opt/.ccache $FULL_TAG
+  fi
 elif [[ $BUILDKITE ]]; then
   echo "This build is running on Buildkite."
+  execute docker run --rm -v $(pwd):/workdir -e JOBS $FULL_TAG
 else
   echo "Shouldn't get here."
 fi  
-# if [[ "$(uname)" == Darwin ]]; then
-#   ccache -s
-#   mkdir -p build && cd build && cmake ..
-#   make -j$(getconf _NPROCESSORS_ONLN)
-# else # linux
-#   execute docker run --rm -v $(pwd):/workdir -v /usr/lib/ccache -v $HOME/.ccache:/opt/.ccache -e CCACHE_DIR=/opt/.ccache $FULL_TAG
-# fi
