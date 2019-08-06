036bea5587a191c6a50055a2e23e1af959fffb8e
Nathan Pierce <nathan.pierce@block.one>
2019-08-06 14:23:57 -0400

    artifact download/upload fixes

--- .cicd/run.sh
+++ .cicd/run.sh
@@ -22,18 +22,14 @@ else # Linux
     execute ./.cicd/generate-base-images.sh
 
     BUILD_COMMANDS="mkdir -p /workdir/build && cd /workdir/build && cmake -DCMAKE_TOOLCHAIN_FILE=/workdir/.cicd/helpers/clang.make -DENABLE_TESTS=ON .. && make -j$JOBS"
-    TEST_COMMANDS="ctest -j$JOBS -V --output-on-failure -T Test"
+    TEST_COMMANDS="cd /workdir/build && ctest -j$JOBS -V --output-on-failure -T Test"
 
     # Docker Run Arguments
     ARGS=${ARGS:-"--rm -v $(pwd):/workdir"}
     # Docker Commands
     if [[ $BUILDKITE ]]; then
-        if ${ENABLE_BUILD:-false}; then
-            append-to-commands $BUILD_COMMANDS
-        fi
-        if ${ENABLE_TEST:-false}; then
-            append-to-commands $TEST_COMMANDS
-        fi
+        [[ $ENABLE_BUILD ]] && append-to-commands $BUILD_COMMANDS
+        [[ $ENABLE_TEST ]] && append-to-commands $TEST_COMMANDS
     elif [[ $TRAVIS ]]; then
         ARGS="$ARGS -v /usr/lib/ccache -v $HOME/.ccache:/opt/.ccache -e JOBS -e CCACHE_DIR=/opt/.ccache"
         COMMANDS="ccache -s && $BUILD_COMMANDS && $TEST_COMMANDS"
@@ -42,10 +38,4 @@ else # Linux
     # Docker Run
     docker-run $COMMANDS
 
-fi
-
-# Buildkite Artifacts
-if [[ $BUILDKITE ]]; then
-    execute tar -pczf build.tar.gz build
-    execute buildkite-agent artifact upload build.tar.gz
 fi
