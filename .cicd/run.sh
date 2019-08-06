449cb9fcc1c7b1e9b9783e8a94b62aa2c2c671c1
Nathan Pierce <nathan.pierce@block.one>
2019-08-06 13:26:47 -0400

    quick fixes

--- .cicd/run.sh
+++ .cicd/run.sh
@@ -22,18 +22,23 @@ else # Linux
     execute ./.cicd/generate-base-images.sh
 
     COMMANDS="echo 'Please provide COMMANDS to run' && exit 1"
-    BUILD_COMMANDS="cmake -DCMAKE_TOOLCHAIN_FILE=/workdir/.cicd/helpers/clang.make -DENABLE_TESTS=ON .. && make -j$JOBS &&"
+    BUILD_COMMANDS="cmake -DCMAKE_TOOLCHAIN_FILE=/workdir/.cicd/helpers/clang.make -DENABLE_TESTS=ON .. && make -j$JOBS"
     TEST_COMMANDS="ctest -j$JOBS -V --output-on-failure -T Test"
 
     # Docker Run Arguments
     ARGS=${ARGS:-"--rm -v $(pwd):/workdir"}
     # Docker Commands
     if [[ $BUILDKITE ]]; then
-        ${ENABLE_BUILD:-false} && COMMANDS="$BUILD_COMMANDS"
-        ${ENABLE_TEST:-false} && COMMANDS="$COMMANDS $TEST_COMMANDS"
+        if $ENABLE_BUILD; then
+            COMMANDS="$BUILD_COMMANDS"
+        elif $ENABLE_TEST; then
+            COMMANDS="$TEST_COMMANDS"
+        elif $ENABLE_BUILD && $ENABLE_TEST; then
+            COMMANDS="$BUILD_COMMANDS && $TEST_COMMANDS"
+        fi
     elif [[ $TRAVIS ]]; then
         ARGS="$ARGS -v /usr/lib/ccache -v $HOME/.ccache:/opt/.ccache -e JOBS -e CCACHE_DIR=/opt/.ccache"
-        COMMANDS="ccache -s && $BUILD_COMMANDS $TEST_COMMANDS"
+        COMMANDS="ccache -s && $BUILD_COMMANDS && $TEST_COMMANDS"
     fi
 
     # Docker Run
