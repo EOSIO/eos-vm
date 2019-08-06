6eaecdcd4bff3cec6bb1e6b8134aa57ed77006ce
Nathan Pierce <nathan.pierce@block.one>
2019-08-06 13:21:58 -0400

    quick fixes

--- .cicd/helpers/docker.sh
+++ .cicd/helpers/docker.sh
@@ -10,5 +10,5 @@ function get_envs() {
 
 function docker-run() {
     get_envs
-    execute docker run $ARGS $evars $FULL_TAG bash -c "$@"
+    execute eval docker run $ARGS $evars $FULL_TAG bash -c \"$@\"
 }
