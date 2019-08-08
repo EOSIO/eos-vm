42becb34ca863e7a6683c1d2d5fa97004512a533
Nathan Pierce <nathan.pierce@block.one>
2019-08-08 16:49:43 -0400

    improvements

--- .cicd/run.sh
+++ .cicd/run.sh
@@ -3,37 +3,35 @@ set -eo pipefail
 . ./.cicd/helpers/general.sh
 . ./$HELPERS_DIR/execute.sh
 
-execute mkdir -p $ROOT_DIR/build
+execute mkdir -p $BUILD_DIR
 
 if [[ $(uname) == Darwin ]]; then
 
     MAC_CMAKE="cmake -DCMAKE_BUILD_TYPE=Release .."
     MAC_MAKE="make -j$JOBS"
     MAC_TEST="ctest -j$JOBS --output-on-failure -T Test"
-    cd $ROOT_DIR
-    ccache -s
-    cd build
-    if [[ $BUILDKITE == true ]]; then
-        if [[ $ENABLE_BUILD == true ]]; then
-            execute $MAC_CMAKE
-            execute $MAC_MAKE
-        elif [[ $ENABLE_TEST == true ]]; then
-            execute $MAC_TEST
-        fi
-    elif [[ $TRAVIS == true ]]; then
-        execute mkdir -p wasms
+    cd $BUILD_DIR
+    execute ccache -s
+    if [[ $ENABLE_BUILD == true ]] || [[ $TRAVIS == true ]]; then
         execute $MAC_CMAKE
         execute $MAC_MAKE
+    fi
+    if [[ $BUILDKITE == true ]]; then
+        [[ $ENABLE_TEST == true ]] && execute $MAC_TEST
+    elif [[ $TRAVIS == true ]]; then
+        execute mkdir -p wasms
         #execute $MAC_TEST
     fi
 
 else # Linux
 
+    MOUNTED_DIR='/workdir'
+
     . ./$HELPERS_DIR/docker.sh
     . ./$HELPERS_DIR/docker-hash.sh
 
-    BUILD_COMMANDS="cd /workdir/build && cmake -DCMAKE_TOOLCHAIN_FILE=/workdir/.cicd/helpers/clang.make -DENABLE_TESTS=ON -DCMAKE_BUILD_TYPE=Release .. && make -j$JOBS"
-    TEST_COMMANDS="cd /workdir/build && ctest -j$JOBS --output-on-failure -T Test"
+    BUILD_COMMANDS="cd $MOUNTED_DIR/build && cmake -DCMAKE_TOOLCHAIN_FILE=$MOUNTED_DIR/.cicd/helpers/clang.make -DENABLE_TESTS=ON -DCMAKE_BUILD_TYPE=Release .. && make -j$JOBS"
+    TEST_COMMANDS="cd $MOUNTED_DIR/build && ctest -j$JOBS --output-on-failure -T Test"
 
     # Docker Run Arguments
     ARGS=${ARGS:-"--rm -v $(pwd):/workdir"}
