8bfbfb6a41dae2520cf8c8b8517cbda6e26ad011
Nathan Pierce <nathan.pierce@block.one>
2019-08-08 10:36:13 -0400

    quick fixes

--- .cicd/run.sh
+++ .cicd/run.sh
@@ -2,30 +2,29 @@
 set -eo pipefail
 . ./.cicd/helpers/general.sh
 . ./$HELPERS_DIR/execute.sh
-
 if [[ $(uname) == Darwin ]]; then
 
-    if [[ $ENABLE_BUILD ]]; then
-        cd $ROOT_DIR
-        ccache -s
-        mkdir -p build
-        cd build
+    cd $ROOT_DIR
+    ccache -s
+    mkdir -p build
+    cd build
+    if [[ $BUILDKITE ]]; then
+        if [[ $ENABLE_BUILD ]]; then
+            execute cmake ..
+            execute make -j$JOBS
+        elif [[ $ENABLE_TEST ]]; then
+            execute ctest -j$JOBS -V --output-on-failure -T Test
+        fi
+    elif [[ $TRAVIS ]]; then
         execute cmake ..
         execute make -j$JOBS
-        cd ..
-    elif [[ $ENABLE_TEST ]]; then
-        ccache -s
-        cd build
-        execute ctest -j$JOBS -V --output-on-failure -T Test
+        #execute ctest -j$JOBS -V --output-on-failure -T Test
     fi
-    
+
 else # Linux
 
     . ./$HELPERS_DIR/docker.sh
     . ./$HELPERS_DIR/docker-hash.sh
-    
-    # Generate Base Images
-    execute ./.cicd/generate-base-images.sh
 
     execute mkdir -p $ROOT_DIR/build
 
@@ -36,10 +35,13 @@ else # Linux
     ARGS=${ARGS:-"--rm -v $(pwd):/workdir"}
     # Docker Commands
     if [[ $BUILDKITE ]]; then
+        # Generate Base Images
+        execute ./.cicd/generate-base-images.sh
         [[ ! -d $ROOT_DIR/build/wasms ]] && execute git clone git@github.com:EOSIO/eos-vm-test-wasms.git $ROOT_DIR/build/wasms # support for private wasm repo (contact Bucky)
         [[ $ENABLE_BUILD ]] && append-to-commands $BUILD_COMMANDS
         [[ $ENABLE_TEST ]] && append-to-commands $TEST_COMMANDS
     elif [[ $TRAVIS ]]; then
+        echo HERE
         execute mkdir $ROOT_DIR/build/wasms
         ARGS="$ARGS -v /usr/lib/ccache -v $HOME/.ccache:/opt/.ccache -e JOBS -e CCACHE_DIR=/opt/.ccache"
         COMMANDS="ccache -s && $BUILD_COMMANDS"
