32f903d82790e3c79be32e31198da7ad69a34e5f
Nathan Pierce <nathan.pierce@block.one>
2019-08-08 09:38:47 -0400

    quick fixes

--- .cicd/run.sh
+++ .cicd/run.sh
@@ -35,7 +35,7 @@ else # Linux
         [[ $ENABLE_TEST ]] && append-to-commands $TEST_COMMANDS
     elif [[ $TRAVIS ]]; then
         ARGS="$ARGS -v /usr/lib/ccache -v $HOME/.ccache:/opt/.ccache -e JOBS -e CCACHE_DIR=/opt/.ccache"
-        COMMANDS="ccache -s && $BUILD_COMMANDS && $TEST_COMMANDS"
+        COMMANDS="ccache -s && $BUILD_COMMANDS"
     fi
 
     # Docker Run
