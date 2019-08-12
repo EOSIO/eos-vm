b449fbb8fbc2ac5c6d5313da35c6ec431ec77ac8
Bucky Kittinger <larry.kittinger@block.one>
2019-08-12 17:07:37 -0400

    add reentry tests

---
+++ tests/reentry_tests.cpp
@@ -0,0 +1,38 @@
+#include <eosio/vm/backend.hpp>
+
+#include <catch2/catch.hpp>
+
+using namespace eosio::vm;
+
+#include "reentry.wasm.hpp"
+
+
+struct test_runner {
+   eosio::vm::backend<test_runner>& bkend;
+   uint32_t test_func_0(uint32_t val) {
+      return bkend.call_with_return(this, "env", "bar")->to_ui32() + 50;
+   }
+   void eosio_assert(uint32_t, uint32_t) {}
+   void* memset(void*, int, int) { return 0; }
+};
+
+using backend_t = eosio::vm::backend<test_runner>;
+using rhf_t     = eosio::vm::registered_host_functions<test_runner>;
+
+TEST_CASE("test reentry", "[reentry]") {
+   wasm_allocator wa;
+   backend_t bkend(reentry_wasm);
+   bkend.set_wasm_allocator(&wa);
+   bkend.initialize();
+   test_runner tr = {bkend};
+   
+   rhf_t::add<test_runner, &test_runner::test_func_0, wasm_allocator>("env", "testfunc0");
+   rhf_t::add<test_runner, &test_runner::eosio_assert, wasm_allocator>("env", "eosio_assert");
+   rhf_t::add<test_runner, &test_runner::memset, wasm_allocator>("env", "memset");
+   rhf_t::resolve(bkend.get_module());
+
+   // level 0
+   CHECK(bkend.call_with_return(nullptr, "env", "foo", (uint32_t)10)->to_ui32() == 52);
+   std::cout << "\n\n\n\n";
+   CHECK(bkend.call_with_return(nullptr, "env", "testbar", (uint32_t)10)->to_ui32() == 110);
+}
