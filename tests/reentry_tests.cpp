4545a38088a5be285255c9b25e8f0ea52f6cbf55
Bucky Kittinger <larry.kittinger@block.one>
2019-08-12 17:44:54 -0400

    finish reentry test

--- tests/reentry_tests.cpp
+++ tests/reentry_tests.cpp
@@ -10,7 +10,11 @@ using namespace eosio::vm;
 struct test_runner {
    eosio::vm::backend<test_runner>& bkend;
    uint32_t test_func_0(uint32_t val) {
+      std::cout << "made it\n";
       return bkend.call_with_return(this, "env", "bar")->to_ui32() + 50;
+   }
+   uint32_t test_func_1(uint32_t val) {
+      return bkend.call_with_return(this, "env", "testbar")->to_ui32() + 50;
    }
    void eosio_assert(uint32_t, uint32_t) {}
    void* memset(void*, int, int) { return 0; }
@@ -27,12 +31,14 @@ TEST_CASE("test reentry", "[reentry]") {
    test_runner tr = {bkend};
    
    rhf_t::add<test_runner, &test_runner::test_func_0, wasm_allocator>("env", "testfunc0");
+   rhf_t::add<test_runner, &test_runner::test_func_1, wasm_allocator>("env", "testfunc1");
    rhf_t::add<test_runner, &test_runner::eosio_assert, wasm_allocator>("env", "eosio_assert");
    rhf_t::add<test_runner, &test_runner::memset, wasm_allocator>("env", "memset");
    rhf_t::resolve(bkend.get_module());
 
    // level 0
-   CHECK(bkend.call_with_return(nullptr, "env", "foo", (uint32_t)10)->to_ui32() == 52);
-   std::cout << "\n\n\n\n";
-   CHECK(bkend.call_with_return(nullptr, "env", "testbar", (uint32_t)10)->to_ui32() == 110);
+   CHECK(bkend.call_with_return(&tr, "env", "foo", (uint32_t)10)->to_ui32() == 52);
+   // level 1
+   CHECK(bkend.call_with_return(&tr, "env", "testbar", (uint32_t)10)->to_ui32() == 160);
+   CHECK(bkend.call_with_return(&tr, "env", "testbaz", (uint32_t)10)->to_ui32() == 513);
 }
