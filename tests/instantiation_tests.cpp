ec4e3df2cab8cae2f57de0d05620a8ec89f1e3ef
Steven Watanabe <steven.watanabe@block.one>
2019-09-26 12:00:35 -0400

    Reduce test execution time.

--- tests/instantiation_tests.cpp
+++ tests/instantiation_tests.cpp
@@ -60,7 +60,7 @@ BACKEND_TEST_CASE("Test instantiating multiple instances of eos-vm",
 
    using backend_t = backend<nullptr_t, TestType>;
    std::list<backend_t> v;
-   for (int i=0; i < 32768; i++)
+   for (int i=0; i < 8193; i++)
       v.emplace_back(test_wasm);
 }
 #pragma GCC pop_options
