fc11ea33871cf6f0b5240ab74613902056aa0b86
Jeffrey Smith II <jeffreyssmith2nd@gmail.com>
2020-04-08 11:09:51 -0400

    Fix Debug build of the tester

--- include/eosio/vm/signals.hpp
+++ include/eosio/vm/signals.hpp
@@ -12,8 +12,8 @@
 
 namespace eosio { namespace vm {
 
-   inline thread_local std::atomic<sigjmp_buf*> signal_dest = ATOMIC_VAR_INIT(nullptr);
-   inline thread_local std::exception_ptr saved_exception{nullptr};
+   inline thread_local static std::atomic<sigjmp_buf*> signal_dest = ATOMIC_VAR_INIT(nullptr);
+   inline thread_local static std::exception_ptr saved_exception{nullptr};
    template<int Sig>
    inline struct sigaction prev_signal_handler;
 
