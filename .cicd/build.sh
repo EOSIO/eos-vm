921fd4765c8641ce5c56714315ea1e14e360e1e1
Zach Butler <zach.butler@block.one>
2020-04-23 15:45:17 -0400

    Remove CICD_DIR variable because it obfuscates what the CI scripts are doing

--- .cicd/build.sh
+++ .cicd/build.sh
@@ -13,10 +13,8 @@ else # Linux
     ARGS=${ARGS:-"--rm -v $(pwd):$MOUNTED_DIR"}
     . .cicd/helpers/docker-hash.sh
     COMMANDS="cd $MOUNTED_DIR/build && cmake -DCMAKE_TOOLCHAIN_FILE=$MOUNTED_DIR/.cicd/helpers/clang.make -DENABLE_TESTS=ON -DCMAKE_BUILD_TYPE=Release .. && make -j$JOBS"
-    # Docker Commands
-    if [[ $BUILDKITE == true ]]; then
-        $CICD_DIR/generate-base-images.sh
-    fi
+    # base-image
+    [[ "$BUILDKITE" == 'true' ]] && .cicd/generate-base-images.sh
     # Load BUILDKITE Environment Variables for use in docker run
     if [[ -f $BUILDKITE_ENV_FILE ]]; then
         evars=""
