0dd04caeda86f037f22ec3068882a4108bf88086
Zach Butler <zach.butler@block.one>
2020-04-23 16:19:28 -0400

    Comments, formatting, and readability

--- .cicd/pipeline-upload.sh
+++ .cicd/pipeline-upload.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 set -e
 echo '+++ :evergreen_tree: Configuring Environment'
-# the TIMEOUT=${TIMEOUT:-45} syntax does not work in the pipeline.yml for timeout values for some reason
 if [[ -z "$TIMEOUT" ]]; then # use defaults
     [[ -z "$BUILD_TIMEOUT" ]] && export BUILD_TIMEOUT='45'
     [[ -z "$TEST_TIMEOUT" ]] && export TEST_TIMEOUT='3'
 else # user override
     [[ -z "$BUILD_TIMEOUT" ]] && export BUILD_TIMEOUT="$TIMEOUT"
     [[ -z "$TEST_TIMEOUT" ]] && export TEST_TIMEOUT="$TIMEOUT"
-fi
+fi # the TIMEOUT=${TIMEOUT:-45} syntax does not work in the pipeline.yml for timeout values for some reason
 echo '+++ :pipeline_upload: Deploying Pipeline Steps'
 buildkite-agent pipeline upload .cicd/pipeline.yml
--- .cicd/test.sh
+++ .cicd/test.sh
@@ -6,21 +6,22 @@ if [[ "$BUILDKITE" == 'true' ]]; then
     buildkite-agent artifact download build.tar.gz . --step "$1"
     tar -xzf build.tar.gz
 fi
+# test
 if [[ $(uname) == 'Darwin' ]]; then
     echo "$ scripts/test.sh"
     scripts/test.sh
-else # Linux
+else # linux
     . .cicd/docker-hash.sh
     # base-image
     [[ "$BUILDKITE" == 'true' ]] && .cicd/generate-base-images.sh
-    # Load BUILDKITE Environment Variables for use in docker run
+    # load BUILDKITE intrinsics into docker container
     if [[ -f $BUILDKITE_ENV_FILE ]]; then
         evars=""
         while read -r var; do
             evars="$evars --env ${var%%=*}"
         done < "$BUILDKITE_ENV_FILE"
     fi
-    # Docker Run with all of the commands we've prepped
+    # docker run
     echo "$ docker run --rm -v \"$(pwd):/eos-vm\" -w /eos-vm $evars $FULL_TAG bash -c 'scripts/test.sh'"
     eval docker run -v "$(pwd):/eos-vm" -w /eos-vm $evars $FULL_TAG bash -c 'scripts/test.sh'
 fi
