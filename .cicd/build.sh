d7e061d1d3ab99d4734a10de09b7ce87bf4c6782
Zach Butler <zach.butler@block.one>
2020-04-23 22:28:00 -0400

    Support running CI scripts locally, where possible

--- .cicd/build.sh
+++ .cicd/build.sh
@@ -7,15 +7,15 @@ else # mac host or linux guest > build
     echo '--- :evergreen_tree: Configuring Environment'
     [[ -z "$JOBS" ]] && export JOBS="$(getconf _NPROCESSORS_ONLN)"
     [[ ! -d build ]] && mkdir build
-    cd build
     # cmake
     CMAKE_COMMAND='cmake -DCMAKE_BUILD_TYPE=Release -DENABLE_TESTS=ON'
-    [[ "$(uname)" == 'Linux' ]] && CMAKE_COMMAND="$CMAKE_COMMAND -DCMAKE_TOOLCHAIN_FILE=/eos-vm/.cicd/clang.make"
+    [[ "$(uname)" == 'Linux' ]] && CMAKE_COMMAND="$CMAKE_COMMAND -DCMAKE_TOOLCHAIN_FILE=$(pwd)/.cicd/clang.make"
     CMAKE_COMMAND="$CMAKE_COMMAND .."
     # make
     MAKE_COMMAND="make -j $JOBS"
     # build
     echo '+++ :hammer_and_wrench: Building'
+    cd build
     echo "$ $CMAKE_COMMAND"
     eval $CMAKE_COMMAND
     echo "$ $MAKE_COMMAND"
--- .cicd/docker.sh
+++ .cicd/docker.sh
@@ -17,13 +17,13 @@ if [[ ! $(docker pull $CONTAINER) ]]; then
 fi
 # pass BUILDKITE intrinsics into docker container
 if [[ -f $BUILDKITE_ENV_FILE ]]; then
-    EVARS='-e DOCKER=true'
+    EVARS=''
     while read -r var; do
         EVARS="$EVARS -e ${var%%=*}"
     done < "$BUILDKITE_ENV_FILE"
 fi
 # docker run
 echo '--- :docker: Running Container'
-DOCKER_COMMAND="docker run --rm -v \"$(pwd):/eos-vm\" -w /eos-vm $EVARS $CONTAINER bash -c '$@'"
+DOCKER_COMMAND="docker run --rm -v \"$(pwd):/eos-vm\" -w /eos-vm $EVARS -e DOCKER=true $CONTAINER bash -c '$@'"
 echo "$ $DOCKER_COMMAND"
 eval $DOCKER_COMMAND
