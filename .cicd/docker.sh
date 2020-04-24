f33bdb36909721ba9ddaa29d7fc7e601fdb0ef46
Zach Butler <zach.butler@block.one>
2020-04-23 21:23:24 -0400

    Refactor CICD BASH scripts

---
+++ .cicd/docker.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -eo pipefail
+echo '--- :arrow_down: Pulling Base-Image'
+export DOCKERFILE="$IMAGE_TAG.dockerfile"
+export HASH="$(sha1sum ".cicd/docker/$DOCKERFILE" | awk '{print $1}')"
+export CONTAINER="docker.io/eosio/ci:eos-vm-$IMAGE_TAG-$HASH"
+echo "SHA-1 hash of \".cicd/docker/$DOCKERFILE\" is $HASH."
+# base-image
+echo "$ docker pull $CONTAINER"
+docker pull $CONTAINER
+export DOCKER_PULL_EXIT_STATUS="$?"
+if [[ "$DOCKER_PULL_EXIT_STATUS" != '0' ]]; then
+    echo '--- :mantelpiece_clock: Building Base-Image'
+    cd .cicd/docker
+    echo "$ docker build -t $CONTAINER -f $DOCKERFILE ."
+    docker build -t $CONTAINER -f $DOCKERFILE .
+    echo "$ docker push $CONTAINER"
+    docker push $CONTAINER
+fi
+# pass BUILDKITE intrinsics into docker container
+if [[ -f $BUILDKITE_ENV_FILE ]]; then
+    EVARS='-e DOCKER=true'
+    while read -r var; do
+        EVARS="$EVARS -e ${var%%=*}"
+    done < "$BUILDKITE_ENV_FILE"
+fi
+# docker run
+echo '--- :docker: Running Container'
+DOCKER_COMMAND="docker run --rm -v \"$(pwd):/eos-vm\" $EVARS $CONTAINER bash -c '$@'"
+echo "$ $DOCKER_COMMAND"
+$DOCKER_COMMAND
--- .cicd/test.sh
+++ .cicd/test.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 set -eo pipefail
-export JOBS=${JOBS:-"$(getconf _NPROCESSORS_ONLN)"}
-# download artifacts
-if [[ "$BUILDKITE" == 'true' ]]; then
+# download artifacts on host
+if [[ "$BUILDKITE" == 'true' && "$DOCKER" != 'true' ]]; then
     buildkite-agent artifact download build.tar.gz . --step "$1"
     tar -xzf build.tar.gz
 fi
 # test
-if [[ $(uname) == 'Darwin' ]]; then
-    echo "$ scripts/test.sh"
-    scripts/test.sh
-else # linux
-    . .cicd/docker-hash.sh
-    # base-image
-    [[ "$BUILDKITE" == 'true' ]] && .cicd/generate-base-images.sh
-    # load BUILDKITE intrinsics into docker container
-    if [[ -f $BUILDKITE_ENV_FILE ]]; then
-        evars=""
-        while read -r var; do
-            evars="$evars --env ${var%%=*}"
-        done < "$BUILDKITE_ENV_FILE"
+if [[ "$(uname)" == 'Linux' && "$DOCKER" != 'true' ]]; then # linux host > run this script in docker
+    .cicd/docker.sh '.cicd/test.sh' $@
+else # mac host or linux guest > test
+    [[ -z "$JOBS" ]] && export JOBS="$(nproc)"
+    TEST_COUNT="$(ctest -N | grep -i 'Total Tests: ' | cut -d ':' -f 2 | awk '{print $1}')"
+    if (( $TEST_COUNT > 0 )); then
+        echo "$TEST_COUNT tests found."
+        # run tests
+        set +e # defer ctest error handling to end
+        echo "$ ctest -j $JOBS --output-on-failure -T Test"
+        ctest  -j $JOBS --output-on-failure -T Test
+        EXIT_STATUS=$?
+        echo 'Done running tests.'
+    else
+        echo "+++ $([[ "$BUILDKITE" == 'true' ]] && echo ':no_entry: ')ERROR: No tests registered with ctest! Exiting..."
+        EXIT_STATUS='1'
     fi
-    # docker run
-    echo "$ docker run --rm -v \"$(pwd):/eos-vm\" -w /eos-vm $evars $FULL_TAG bash -c 'scripts/test.sh'"
-    eval docker run -v "$(pwd):/eos-vm" -w /eos-vm $evars $FULL_TAG bash -c 'scripts/test.sh'
 fi
+exit $EXIT_STATUS
